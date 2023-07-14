88fd0a763d1b8e3edb672180f4ca74886f37fe94
Chris Cooksey <ccooksey@mindspring.com>
2023-07-13 23:12:44 -0400

    Updated to work on a publicly hosted server

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 // Example code followed for this project was found at:
 // https://blog.logrocket.com/implement-oauth-2-0-node-js/
 
-// Database imports
+const fs = require('fs');
+const https = require('https');
+const dotenv = require('dotenv');
+
 // Must be run with "npm run start:prod" or "npm run start:dev" for this to work
-const path = require('node:path');
-require('dotenv').config({ path: `./.env.${process.env.NODE_ENV}` });
-const database = require(process.env.SERVER_DATABASE_WRAPPER);
+dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
+
+// Database imports
+const database = require(process.env.SERVER_DB_WRAPPER);
 const tokenDB = require("./db/tokenDB")(database);
 const userDB = require("./db/userDB")(database);
+
+// OAuth imports
+const oAuthService = require('./auth/tokenService')(userDB, tokenDB);
+const oAuth2Server = require('node-oauth2-server');
+
+// Express
+const express = require("express");
+const cors = require('cors');
 
 // Log our configuration
 console.log(`NODE_ENV = ${process.env.NODE_ENV}`);
 database.identifyDBWrapper();
+console.log(`Server located at ${__dirname}`);
 
-// OAuth imports
-const oAuthService = require("./auth/tokenService")(userDB, tokenDB);
-const oAuth2Server = require("node-oauth2-server");
-
-// cors
-const cors = require('cors');
-
-// Express
-const express = require("express");
 const app = express();
 
 // Overwrite express's oauth server with our implementation.
@@ -33,28 +37,39 @@ app.oauth = oAuth2Server({
 
 // Authorization routing (/auth)
 const authenticator = require("./auth/authenticator")(userDB, tokenDB);
-const routes = require("./auth/routes")(
-    express.Router(),
-    app,
-    authenticator);
+const routes = require("./auth/routes")(express.Router(), app, authenticator);
 
 // Test routing (/test)
 const testAPIService = require("./test/testAPIService.js");
-const testAPIRoutes = require("./test/testAPIRoutes.js")(
-    express.Router(),
-    app,
-    testAPIService
-);
+const testAPIRoutes = require("./test/testAPIRoutes.js")(express.Router(), app, testAPIService);
 
-app.use(cors());
+// Restrict CORS access as much as desired
+const corsOptions = { 
+    origin: process.env.SERVER_ALLOWED_ORIGINS,
+    methods: 'POST, DELETE, OPTIONS' 
+}
+app.use(cors(corsOptions));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(app.oauth.errorHandler());
 app.use("/auth", routes);
 app.use("/test", testAPIRoutes);
 
-// Let's listen! Seems like oauth2 does not have a standard port number
-const port = 9443;
-app.listen(port, () => {
-    console.log(`listening on port ${port}`);
-});
+function Development() {
+    return (!process.env.NODE_ENV || process.env.NODE_ENV === 'development');
+}
+
+// Let's listen!
+if (Development()) {
+    app.listen(process.env.SERVER_PORT_HTTP, () => {
+        console.log(`HTTP server listening on port ${process.env.SERVER_PORT_HTTP}`)
+    });
+} else {
+    const options = {
+        key: fs.readFileSync(process.env.SERVER_PRIVATE_KEY_PATH),
+        cert: fs.readFileSync(process.env.SERVER_FULL_CHAIN_PATH)
+    }
+    https.createServer(options, app).listen(process.env.SERVER_PORT_HTTPS, () => {
+        console.log(`HTTPS server listening on port ${process.env.SERVER_PORT_HTTPS}`)
+    });
+}
